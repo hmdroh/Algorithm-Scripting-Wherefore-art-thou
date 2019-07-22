d80111427a80176f817b1e30876bcaa8d3a5e2b8
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2019-07-22 16:00:41 -0700

    solved

--- unsolved.js
+++ unsolved.js
@@ -1,11 +1,30 @@
 function whatIsInAName(collection, source) {
-    // What's in a name?
-    var arr = [];
-    // Only change code below this line
-    
-    
-    // Only change code above this line
-    return arr;
+  // What's in a name?
+  var arr = [];
+ 
+  // Only change code below this line
+  for(var i =0; i <collection.length; i++){
+    var currOb = collection[i];
+    var checkable = true;
+    for (let key in source){
+      if(currOb[key] !== source[key]){
+        checkable = false;
+      }
+    }
+    if(checkable){
+      arr.push(collection[i])
+    }
   }
   
-  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" }, 
+    { first: "Mercutio", last: null }, 
+    { first: "Tybalt", last: "Capulet" }
+  ]
+
+, { last: "Capulet" });
